db2b83357de9ac8d17e6ad32ab5c30671db35448
Rickytta <rehema_ru@yahoo.ca>
2019-07-12 20:05:51 +0200

    ch-167188094-writing-API

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# project-x
+# project-x - Propert pro lite 
+
+ <a href="https://travis-ci.org/Rickytta/project-x"><img src="https://travis-ci.org/Rickytta/project-x.svg?branch=develop" alt="Build Status"/> </a>
+<a href="https://codeclimate.com/github/Rickytta/project-x/maintainability"><img src="https://api.codeclimate.com/v1/badges/97ccfc79d21d78d114ad/maintainability" /></a>
---
+++ src/helpers/validate.js
@@ -0,0 +1,118 @@
+import { error } from '../usingJSObject/data/error';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+const getNewId = (array) => {
+
+    if (array.length > 0) {
+        let n;
+        let idValue = array[array.length - 1].id;
+        n = idValue + 1;
+        return n;
+
+    } else {
+        return 234013005001;
+    }
+
+};
+
+const getSubId = (array) => {
+
+    if (array.length > 0) {
+        let n;
+        let idValue = array[array.length - 1].id;
+        n = idValue + 1;
+        return n;
+
+    } else {
+        return 43501;
+    }
+
+};
+const newDate = () => new Date().toLocaleString();
+
+const mustBeInArray = (array, id) => {
+    return new Promise((resolve, reject) => {
+        const row = array.find(r => r.id == id);
+        if (!row) {
+            reject(error.user_404);
+        }
+        resolve(row);
+    });
+};
+
+const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, 10, function(err, hash) {
+            resolve(hash);
+            reject(err);
+        });
+    });
+
+};
+
+const comparePassword = (hashpassword, password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, hashpassword, function(err, bool) {
+            resolve(bool);
+            reject(err);
+        });
+    });
+
+};
+
+
+const adminDb = (array, email) => {
+    // eslint-disable-next-line no-unused-vars
+    return new Promise((resolve, reject) => {
+        const db = array.find(r => r.email === email);
+        resolve(db);
+    });
+};
+
+const typeSearch = (array, type) => {
+    return new Promise((resolve, reject) => {
+        const db = array.filter(r => r.type === type);
+        if (!db) {
+            reject(error.property_404);
+        }
+        resolve(db);
+    });
+};
+
+/**
+ * isValidEmail helper method
+ * @param {string} email
+ * @returns {Boolean} True or False
+ */
+
+const isValidEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+};
+
+/**
+ * Gnerate Token
+ * @param {string} id
+ * @returns {string} token
+ */
+const generateToken = (id) => {
+    return new Promise((resolve, reject) => {
+        const token = jwt.sign({ userId: id }, process.env.SECRET_KEY, { expiresIn: '7d' });
+        if (token) resolve(token);
+        if (!token) reject({ err: 'could not assign a token' });
+    });
+
+};
+
+const emailToken = (rand) => {
+    return new Promise((resolve, reject) => {
+        const token = jwt.sign({ code: rand }, process.env.SECRET_KEY, { expiresIn: '5m' });
+        if (token) resolve(token);
+        if (!token) reject({ err: 'could not assign a token' });
+    });
+
+};
+
+
+
+
+export { getNewId, emailToken, newDate, mustBeInArray, hashPassword, getSubId, adminDb, typeSearch, isValidEmail, generateToken, comparePassword };
